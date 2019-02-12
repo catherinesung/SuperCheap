1de43ac8bd1e52e5154e89bce2d09cae3a519e62
manfanny829 <manfanny829@gmail.com>
2019-02-12 23:40:18 +0800

    add barcode and search bar

--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -4,9 +4,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
+import { BarcodeScanner} from '@ionic-native/barcode-scanner/ngx';
+import { Platform } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 @NgModule({
   imports: [
+
     IonicModule,
     CommonModule,
     FormsModule,
@@ -14,4 +19,29 @@ import { Tab1Page } from './tab1.page';
   ],
   declarations: [Tab1Page]
 })
-export class Tab1PageModule {}
+export class Tab1PageModule {
+  scannedData: {};
+  constructor(
+      private platform: Platform,
+      private splashScreen: SplashScreen,
+      private statusBar: StatusBar,
+      private barcodeScanner: BarcodeScanner
+  ) {
+    this.initializeApp();
+  }
+  initializeApp() {
+    this.platform.ready().then(() => {
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+    });
+  }
+
+  scanCode() {
+    this.barcodeScanner.scan().then(barcodeData => {
+      alert('Barcode data ' + JSON.stringify(barcodeData));
+      this.scannedData = barcodeData;
+    }).catch(err => {
+      console.log('Error', err);
+    });
+  }
+}
