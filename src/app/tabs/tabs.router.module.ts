4f1d0703bf7659ef636789af9835b55b767b97d5
catherinesung <catherinesung@users.noreply.github.com>
2019-02-19 00:35:30 +0800

    Revert "link tab page to the tabbar of product page"
    This reverts commit a9b013f0f9886ec88888c92cf5be2a8dd7cc1a42.

--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -64,31 +64,6 @@ const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
-        redirectTo: '/tabs/tab1',
-        pathMatch: 'full'
-      },
-      {
-        path: 'tab2',
-        redirectTo: '/tabs/tab2',
-        pathMatch: 'full'
-      },
-      {
-        path: 'tab3',
-        redirectTo: '/tabs/tab3',
-        pathMatch: 'full'
-      },
-      {
-        path: 'tab4',
-        redirectTo: '/tabs/tab4',
-        pathMatch: 'full'
-      },
-      {
-        path: 'tab5',
-        redirectTo: '/tabs/tab5',
-        pathMatch: 'full'
-      },
-      {
         path: '',
         loadChildren: '../product/product.module#ProductPageModule'
       }
