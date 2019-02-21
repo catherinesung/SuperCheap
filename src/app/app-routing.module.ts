6c5d11ce4ce0c651a3b9f3d9d00317c1bf0ad996
manfanny829 <manfanny829@gmail.com>
2019-02-21 09:02:29 +0800

    add adv card and category card

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'product', loadChildren: './product/product.module#ProductPageModule' }
+
 ];
 @NgModule({
   imports: [
