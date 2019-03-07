b099373006a731edb54173faca2ce6cb9bd50253
manfanny829 <manfanny829@gmail.com>
2019-03-07 15:36:56 +0800

    result layout

--- src/app/Item.ts
+++ src/app/Item.ts
@@ -2,4 +2,6 @@ export class Item {
     id: number;
     name: string;
     category: string;
+    picture: string;
+    price: number;
 }
--- src/app/Items.ts
+++ src/app/Items.ts
@@ -1,7 +1,7 @@
 import { Item } from './Item';
 
 export const ITEMS: Item[] = [
-    { id: 1, name: 'water', category: 'drinks' },
-    { id: 2, name: 'pie', category: 'food' },
-    { id: 3, name: 'coke', category: 'drinks' }
+    { id: 1, name: 'water', category: 'drinks', picture: './assets/image/water.png', price: 7.9 },
+    { id: 2, name: 'pie', category: 'food' , picture: './assets/image/pie.png', price:17.9},
+    { id: 3, name: 'coke', category: 'drinks', picture: './assets/image/coke.png', price:10 }
     ];
