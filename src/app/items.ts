cefe652f410ab4068ea28f3bc7b225ee52cf2409
catherinesung <catherinesung@users.noreply.github.com>
2019-03-20 02:26:14 +0800

    changed back to original items.ts in case mts needs it

--- src/app/items.ts
+++ src/app/items.ts
@@ -1,3 +1,7 @@
-import { Item } from './item';
+import { Item } from './Item';
 
-export const ITEMS: Item[] = [];
+export const ITEMS: Item[] = [
+    { id: 1, name: 'water', category: 'drinks' },
+    { id: 2, name: 'pie', category: 'food' },
+    { id: 3, name: 'coke', category: 'drinks' }
+];
