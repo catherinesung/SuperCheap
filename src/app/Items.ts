c81fe05e3b48c290a674147a43ac9393ddec6f8d
newtonyan <35562580+newtonyan@users.noreply.github.com>
2019-03-21 13:51:47 +0800

    Revert "remove items.ts"
    This reverts commit 81e6118053a1723f79c458b8a7b81e307cd1647c.

---
+++ src/app/Items.ts
@@ -0,0 +1,7 @@
+import { Item } from './item';
+
+export const ITEMS: Item[] = [
+    { id: 1, name: 'water', category: 'drinks' },
+    { id: 2, name: 'pie', category: 'food' },
+    { id: 3, name: 'coke', category: 'drinks' }
+];
