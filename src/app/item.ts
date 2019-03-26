70499c003382b51e65d076e7169b643f8dde182e
manfanny829 <manfanny829@gmail.com>
2019-03-26 12:53:28 +0800

    Merge branch 'master' of https://github.com/catherinesung/SuperCheap

--- src/app/item.ts
+++ src/app/item.ts
@@ -2,30 +2,6 @@ export class Item {
     id: number;
     name: string;
     category: string;
-    constructor(
-        barcode: string,
-        name_tc: string,
-        name_en: string,
-        brand_tc: string,
-        brand_en: string,
-        type_tc: string,
-        type_en: string,
-        price_aeon: string,
-        price_dch: string,
-        price_marketplace: string,
-        price_parknshop: string,
-        price_wellcome: string,
-        price_waston: string,
-        remark_tc_aeon: string,
-        remark_tc_dch: string,
-        remark_tc_marketplace: string,
-        remark_tc_parknshop: string,
-        remark_tc_wellcome: string,
-        remark_tc_waston: string,
-        remark_en_aeon: string,
-        remark_en_dch: string,
-        remark_en_marketplace: string,
-        remark_en_parknshop: string,
-        remark_en_wellcome: string,
-        remark_en_waston: string) {}
+    picture: string;
+    price: number;
 }
