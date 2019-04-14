f13f7327f042c3257fcbdbecc3b94222cc339858
manfanny829 <manfanny829@gmail.com>
2019-04-14 21:02:04 +0800

    Revert "lalala"
    This reverts commit e0e04e681fe7cb648f93f577894f80f82b7f6fa7.

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {LocationService} from '../location.service';
 import {Storeinfo} from '../storeinfo';
+<<<<<<< HEAD
 import { NativeGeocoder,
     NativeGeocoderForwardResult, NativeGeocoderOptions } from '@ionic-native/native-geocoder';
+
+
+=======
+>>>>>>> parent of c000959... bug
 
 @Component({
     selector: 'app-tab2',
@@ -11,11 +16,9 @@ import { NativeGeocoder,
 })
 
 export class Tab2Page implements OnInit {
-    constructor(private locationService: LocationService, private nativeGeocoder: NativeGeocoder) {
+    constructor(private locationService: LocationService) {
     }
     storeinfos: Storeinfo[];
-    long: string;
-    lat: string;
     ngOnInit(): void {
         const options: NativeGeocoderOptions = {
             useLocale: true,
@@ -25,11 +28,14 @@ export class Tab2Page implements OnInit {
             (res: Storeinfo[]) => {
                 this.storeinfos = res;
             });
+<<<<<<< HEAD
         this.nativeGeocoder.forwardGeocode('Berlin', options)
             .then((coordinates: NativeGeocoderForwardResult[]) => {
                 this.long = coordinates[0].longitude;
                 this.lat = coordinates[0].latitude;
             })
             .catch((error: any) => console.log(error));
+=======
+>>>>>>> parent of c000959... bug
     }
 }
