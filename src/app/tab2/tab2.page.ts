15a01237b3c4c8404237e4c86d949967d37109de
manfanny829 <manfanny829@gmail.com>
2019-04-14 21:02:35 +0800

    Revert "bug"
    This reverts commit c000959cc8bd0e465269b14e1c0774a0b1dbf8e2.

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {LocationService} from '../location.service';
 import {Storeinfo} from '../storeinfo';
-import { NativeGeocoder,  NativeGeocoderForwardResult } from '@ionic-native/native-geocoder';
-
 
 @Component({
     selector: 'app-tab2',
@@ -11,9 +9,7 @@ import { NativeGeocoder,  NativeGeocoderForwardResult } from '@ionic-native/nati
 })
 
 export class Tab2Page implements OnInit {
-    lat: string;
-    long: string;
-    constructor(private locationService: LocationService, private nativeGeocoder: NativeGeocoder) {
+    constructor(private locationService: LocationService) {
     }
     storeinfos: Storeinfo[];
     ngOnInit(): void {
@@ -21,11 +17,5 @@ export class Tab2Page implements OnInit {
             (res: Storeinfo[]) => {
                 this.storeinfos = res;
             });
-        this.nativeGeocoder.forwardGeocode('Berlin')
-            .then((coordinates: NativeGeocoderForwardResult[]) => {
-                this.long = coordinates[0].longitude;
-                this.lat = coordinates[0].latitude;
-            })
-            .catch((error: any) => console.log(error));
     }
 }
