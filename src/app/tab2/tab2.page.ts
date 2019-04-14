5247c5bec69f1c610c882d672156066dcac9b243
manfanny829 <manfanny829@gmail.com>
2019-04-14 21:02:53 +0800

    Revert "bug"
    This reverts commit 1b3f8fd06a883cc0b4be828a8657122c78198f49.

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {LocationService} from '../location.service';
 import {Storeinfo} from '../storeinfo';
 
+
 @Component({
     selector: 'app-tab2',
     templateUrl: 'tab2.page.html',
@@ -17,5 +18,5 @@ export class Tab2Page implements OnInit {
             (res: Storeinfo[]) => {
                 this.storeinfos = res;
             });
-    }
+        }
 }
