846cc1db71ec26c83df59d25158933f899800eed
newtonyan <35562580+newtonyan@users.noreply.github.com>
2019-04-18 23:20:40 +0800

    change badge color

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -24,23 +24,6 @@ export class Tab4Page implements OnInit {
   supermarket = 'min_price';
   delivery = false;
   deliveryDetails = [];
-  /*options : InAppBrowserOptions = {
-    location : 'yes',//Or 'no'
-    hidden : 'yes', //Or  'yes'
-    clearcache : 'yes',
-    clearsessioncache : 'yes',
-    zoom : 'yes',//Android only ,shows browser zoom controls
-    hardwareback : 'yes',
-    mediaPlaybackRequiresUserAction : 'no',
-    shouldPauseOnSuspend : 'no', //Android only
-    closebuttoncaption : 'Close', //iOS only
-    disallowoverscroll : 'no', //iOS only
-    toolbar : 'yes', //iOS only
-    enableViewportScale : 'no', //iOS only
-    allowInlineMediaPlayback : 'no',//iOS only
-    presentationstyle : 'pagesheet',//iOS only
-    fullscreen : 'yes',//Windows only
-  };*/
 
   @ViewChild('slidingList') slidingList: IonList;
 
