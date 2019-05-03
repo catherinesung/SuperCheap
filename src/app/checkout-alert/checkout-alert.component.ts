dd2a632d2f06b89936eb13bb9cb6b1f1773f68b2
Newton Yan <yankaiyin@hotmail.com>
2019-05-03 19:58:46 +0800

    place order

--- src/app/checkout-alert/checkout-alert.component.ts
+++ src/app/checkout-alert/checkout-alert.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CartService} from '../cart.service';
+import {SafariViewController} from '@ionic-native/safari-view-controller/ngx';
+import {LoadingController, ToastController} from '@ionic/angular';
 
 @Component({
   selector: 'app-checkout-alert',
@@ -25,7 +27,9 @@ export class CheckoutAlertComponent implements OnInit {
   //total[5]:wellcome
   //total[6]:waston
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService,
+              private safariViewController: SafariViewController,
+              public loadingController: LoadingController) { }
 
   ngOnInit() {
     this.cart = this.cartService.getCart();
@@ -60,6 +64,72 @@ export class CheckoutAlertComponent implements OnInit {
       temp += +products.item.displayPrice [1] * products.quantity;
     }
     return temp;
+  }
+
+  async placeOrder(){
+    const loading = await this.loadingController.create({
+      message: '正在將貨品加入百佳網上商店購物車'
+    });
+    await loading.present();
+
+    const shoppingCart = 'https://www.parknshop.com/en/shoppingCart';
+    for (let products of this.parknshop) {
+      const url = 'https://www.parknshop.com/en/cart/add?productCodePost=' + products.item.bp.substr(3) + '&qty=' + products.quantity;
+      console.log(url);
+      await new Promise(resolve => this.openNewTab(url, true)
+          .then(() => resolve()));
+    }
+    loading.dismiss().then(
+        () => {
+          for (const products of this.cart){
+            if (products.item.displayPrice[0] === 'price_parknshop'){
+              this.cartService.removeProduct(products);
+            }
+          }
+          this.openNewTab(shoppingCart, false);
+        }
+    );
+  }
+
+  openNewTab(url, hidden: boolean){
+    return new Promise ((resolve, reject) => {
+      this.safariViewController.isAvailable()
+          .then((available: boolean) => {
+                if (available) {
+                  this.safariViewController.show({
+                    url: url,
+                    hidden: hidden,
+                    animated: false,
+                    transition: 'curl',
+                    enterReaderModeIfAvailable: false,
+                    tintColor: '#ff0000'
+                  })
+                      .subscribe((result: any) => {
+                            if (result.event === 'opened') {
+                              // console.log('Opened');
+                              console.log(url + 'opened');
+                            }
+                            else if(result.event === 'loaded') {
+                              // console.log('Loaded');
+                              resolve();
+                              console.log(url + 'loaded');
+                            }
+                            else if(result.event === 'closed') {
+                              console.log('Closed');
+                            }
+                          },
+                          (error: any) => {
+                            console.error(error);
+                            reject();
+                          }
+                      );
+
+                } else {
+                  // use fallback browser, example InAppBrowser
+                }
+              }
+          );
+    });
   }
 
 }
