d76cb49a65b020d2276334bbb4e324d3f29cf7e0
catherinesung <catherinesung@users.noreply.github.com>
2019-05-04 16:56:00 +0800

    upload user record to database

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -3,7 +3,8 @@ import { AuthService, FacebookLoginProvider, GoogleLoginProvider } from 'angular
 import { HttpClient } from '@angular/common/http';
 
 import {User} from '../user';
-import {UserService, user} from '../user.service';
+import {UserService} from '../user.service';
+import {UserRecordService} from '../user-record.service';
 
 @Component({
   selector: 'app-tab4',
@@ -17,7 +18,7 @@ export class Tab5Page implements OnInit {
   couponpage: boolean;
 
   constructor( private socialAuthService: AuthService, private http: HttpClient,
-               private userservice: UserService) {}
+               private userservice: UserService, private userrecordservice: UserRecordService) {}
 
   ngOnInit(): void {
       this.login = false;
@@ -36,6 +37,7 @@ export class Tab5Page implements OnInit {
       this.theuser = userData;
       this.userservice.postUser(this.theuser).subscribe();
       this.login = true;
+      this.userrecordservice.userIdentity(this.theuser.provider, this.theuser.id);
     });
   }
 
