0934d413a629f6225a526c8d52d4365fe1be031b
manfanny829 <manfanny829@gmail.com>
2019-05-05 16:34:58 +0800

    Revert "Revert "Revert "Revert "Merge branch 'master' into mts""""
    This reverts commit 4bfa4b82df2f43f2fb1903a94d248b1dad6d92ae.

--- src/app/tab5/tab5.page.ts
+++ src/app/tab5/tab5.page.ts
@@ -5,6 +5,8 @@ import { HttpClient } from '@angular/common/http';
 import {User} from '../user';
 import {UserService} from '../user.service';
 import {UserRecordService} from '../user-record.service';
+import {InAppBrowser} from '@ionic-native/in-app-browser/ngx';
+import {SafariViewController} from '@ionic-native/safari-view-controller/ngx';
 
 @Component({
   selector: 'app-tab4',
@@ -15,21 +17,23 @@ import {UserRecordService} from '../user-record.service';
 export class Tab5Page implements OnInit {
   theuser: User;
   login: boolean;
-  couponpage: boolean;
+  parknshop: boolean;
 
   constructor( private socialAuthService: AuthService, private http: HttpClient,
-               private userservice: UserService, private userrecordservice: UserRecordService) {}
+               private userservice: UserService, private userrecordservice: UserRecordService,
+               private theInAppBrowser: InAppBrowser, private safariViewController: SafariViewController) {}
 
   ngOnInit(): void {
       this.login = false;
-      this.couponpage = false;
+      this.parknshop = false;
   }
 
   public socialSignIn(socialPlatform: string) {
     let socialPlatformProvider;
     if (socialPlatform === 'facebook') {
       socialPlatformProvider = FacebookLoginProvider.PROVIDER_ID;
-    } else if (socialPlatform === 'google') {
+    }
+    if (socialPlatform === 'google') {
       socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
     }
     this.socialAuthService.signIn(socialPlatformProvider).then((userData) => {
@@ -41,15 +45,58 @@ export class Tab5Page implements OnInit {
     });
   }
 
-  coupon(): void {
-    this.couponpage = true;
+  openParknShop(): void {
+    this.parknshop = true;
+    const shoppingCart = 'https://www.parknshop.com/zh-hk/shoppingCart';
+    this.openNewTab(shoppingCart, false);
   }
 
-  couponout(): void {
-    this.couponpage = false;
+  backtoAC(): void {
+    this.parknshop = false;
   }
 
   logout(): void {
     this.login = false;
+  }
+
+  openNewTab(url, hidden: boolean){
+    return new Promise ((resolve, reject) => {
+      this.safariViewController.isAvailable()
+          .then((available: boolean) => {
+                if (available) {
+                  this.safariViewController.show({
+                    url: url,
+                    hidden: hidden,
+                    animated: false,
+                    transition: 'curl',
+                    enterReaderModeIfAvailable: false,
+                    tintColor: '#ff0000'
+                  })
+                      .subscribe((result: any) => {
+                            if (result.event === 'opened') {
+                              // console.log('Opened');
+                              console.log(url + 'opened');
+                            }
+                            else if(result.event === 'loaded') {
+                              // console.log('Loaded');
+                              resolve();
+                              console.log(url + 'loaded');
+                            }
+                            else if(result.event === 'closed') {
+                              console.log('Closed');
+                            }
+                          },
+                          (error: any) => {
+                            console.error(error);
+                            reject();
+                          }
+                      );
+
+                } else {
+                  // use fallback browser, example InAppBrowser
+                }
+              }
+          );
+    });
   }
 }
--- src/app/user-record.ts
+++ src/app/user-record.ts
@@ -1,4 +1,3 @@
-
 export interface UserRecord {
     provider: string;
     id: string;
